3f8ff66f340019fe33259024b47d39e9c37b20a9
Huy Quang Do <quangdh@vn-sis.com>
2020-07-15 22:15:14 +0700

    disable apps

--- src/utils/DataUtils.ts
+++ src/utils/DataUtils.ts
@@ -23,23 +23,24 @@ export function getListGame(): any[] {
 }
 
 export function getListApp(): any[] {
-  const posts = ((context) => {
-    const keys = context.keys();
-    const values = keys.map(context);
+  // const posts = ((context) => {
+  //   const keys = context.keys();
+  //   const values = keys.map(context);
 
-    const data = keys.map((key, index) => {
-      let slug = key.replace(/^.*[\\\/]/, "").slice(0, -3);
-      const value = values[index];
-      //@ts-ignore
-      const document = matter(value.default);
-      return {
-        frontmatter: document.data,
-        markdownBody: document.content,
-        slug,
-      };
-    });
-    return data;
-  })(require.context("src/data/apps", true, /\.md$/));
-  //@ts-ignore
-  return posts;
+  //   const data = keys.map((key, index) => {
+  //     let slug = key.replace(/^.*[\\\/]/, "").slice(0, -3);
+  //     const value = values[index];
+  //     //@ts-ignore
+  //     const document = matter(value.default);
+  //     return {
+  //       frontmatter: document.data,
+  //       markdownBody: document.content,
+  //       slug,
+  //     };
+  //   });
+  //   return data;
+  // })(require.context("src/data/apps", true, /\.md$/));
+  // //@ts-ignore
+  // return posts;
+  return [];
 }
